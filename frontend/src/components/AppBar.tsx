4c5ab2c631f50b701fa5de29c9fffca54c212902
Arush Kumar <arush.beast012@gmail.com>
2024-12-19 20:02:00 +0530

    CSS Improved

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -26,7 +26,7 @@ export const AppBar = () => {
       </Link>
       <div className="flex">
         <Link to={'/publish'}>
-        <button type="button" className="text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2 text-center me-2 mb-2 mr-3">Publish</button>
+        <button type="button" className="text-white bg-gradient-to-r from-green-300 to-green-600 hover:from-green-400 hover:to-green-700 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2 text-center me-2 mb-2 mr-3">Publish</button>
 
         </Link>
         <div onClick={toggleDropdown} className="cursor-pointer">
