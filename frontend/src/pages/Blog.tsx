b8bc6cca7179e9a53639ae06dd287739aa4ab5d5
Arush Kumar <arush.beast012@gmail.com>
2024-12-19 21:13:44 +0530

    Improved

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -3,6 +3,9 @@ import {  useBlog } from "../hooks"
 import { FullBlog } from "../components/FullBlog"
 import { Spinner } from "../components/Spinner"
 import { AppBar } from "../components/AppBar"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { BACKEND_URL } from "../config"
 
 
 export const Blog = () => {
@@ -10,10 +13,29 @@ export const Blog = () => {
     const {loading, blog} = useBlog({
         id: String(id)
     })
+    const [name,setName] = useState('')
+
+    useEffect(() => {
+      (async () => {
+        try {
+          const response = await axios.get(`${BACKEND_URL}/api/v1/blog/profile`,{
+            headers:{
+                Authorization:localStorage.getItem('token')
+            }
+        });
+
+          if(response.data.success){
+            setName(response.data.name)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      })()
+    },[])
 
     if(loading){
         return <div>
-            <AppBar />
+            <AppBar name={name}/>
              <div className="flex justify-center items-center h-[80vh] w-screen">
             <Spinner />
         </div>
@@ -22,7 +44,7 @@ export const Blog = () => {
     }
     return <div>
         
-        <AppBar />
+        <AppBar name={name}/>
         {/* @ts-ignore */}
         <FullBlog blog={blog}/>
     </div>
